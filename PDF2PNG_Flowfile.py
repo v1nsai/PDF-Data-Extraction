fb993fe91d3b7c8e8d8b97812793fd6830f5b888
Drew Riffle <andrewlriffle@protonmail.com>
2018-08-13 19:33:38 -0400

    Reads PDF from stdin, converts to PNG and saves.  Need to figure out how to handle pages and send only the ones we want to stdout

--- PDF2PNG_Flowfile.py
+++ PDF2PNG_Flowfile.py
@@ -12,46 +12,26 @@ import os
 import re
 import sys
 import io
+    
+def PDF2PNG(flowFile, resolution):
 
-def findFileName(file):
-    """
-    Removes all .* endings from a file to get just the filename
-    """
-    (name, ext) = os.path.splitext(file)
-    return(name)
-    
-def PDF2PNG(flowFile, output_directory, resolution = 500):
-    """
-    Picks up all PDF files in a folder and outputs them as a PNG image
-    with 800 resolution as default. This can be edited
-    @@param folder_location folder location for where PDFs are stored
-    @@param resolution specify density of image conversion
-    """
-    
-    #make a subdirectory to put the image exports if the directory
-    #does not already exist
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)    
-        
     #convert pdf to an image
-    with Image(filename=flowFile, resolution = resolution) as img:
-        #find the filename
-        filename = findFileName(flowFile)
+    with Image(file=flowFile, resolution=resolution) as img:
         
         #loop through pages of PDF
         for i, page in enumerate(img.sequence):
             with Image(page) as im:
-                im.alpha_channel = False
-            
+                img.alpha_channel = False
+                img.format = 'png'
                 #save PNG export in subfolder
-                im.save(filename= output_directory + 
-                        '/' + filename + 'page-%s.png' % i)
-        
-        #delete file so NiFi doesn't repeat process on it
-        os.remove(filename + ".pdf")
+                img.save(filename='test.png')
+
+        # img.save(flowFile)
+        #
+        # sys.stdout.buffer.write(flowFile.getvalue())
 
 if __name__ == '__main__':
-    folder_location = 'C:\\Users\\Brandon Croarkin\\Documents\\GreenZone\\OCR\\NiFiTesting'
+    folder_location = '/Users/doctor_ew/PycharmProjects/PDF-Data-Extraction'
     output_directory = 'PythonPNGs'
     
     # Put the incoming FlowFile into a dataframe
@@ -61,5 +41,5 @@ if __name__ == '__main__':
     #file = 'i-9_02-02-09(Filled)(OCR).pdf'
     
     #convert PDFs to images
-    PDF2PNG(flowFile, output_directory, resolution = 200)
+    PDF2PNG(flowFile, resolution=200)
 
