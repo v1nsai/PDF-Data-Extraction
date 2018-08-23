795125d7ba4101ec1d6e82c2390a123c96ba3018
Dr Ew <jamzsabb@gmail.com>
2018-08-23 13:37:03 -0400

    Minor adjustments to coordinates

--- nocr/parse_results.py
+++ nocr/parse_results.py
@@ -3,21 +3,11 @@ from pandas.io.json import json_normalize
 import sys
 import json
 from pathlib import Path
-import re
 
 # Put the incoming FlowFile into a dataframe
 flowFile = sys.stdin.read()
 data = json.loads(flowFile)
 df = json_normalize(data)
-
-# df = pd.read_csv('/Users/doctor_ew/PycharmProjects/I9PDFExtractor/nocr/results.csv')
-
-attlist = ['citizen', 'alien', 'national', 'resident']
-if set(attlist) == set(df.columns):
-    for answer in attlist:
-        if bool(re.search(r'^X', df.ix[0, answer])):
-            df.ix[0, 'Attestation'] = answer
-        df = df.drop(answer, 1)
 
 # Check for previous output and append to it or create a new output file
 results_file = Path('/data/fast/hortonworks/PDF-Data-Extraction/output/results.csv')
